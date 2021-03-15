53fb901b86f059e015fbe57c8be6aeefb5a13cff
orejametov-dev <orejametov777@gmail.com>
2021-03-15 21:17:25 +0500

    small fixes

--- app/Http/Middleware/CheckGatewayAuthUser.php
+++ app/Http/Middleware/CheckGatewayAuthUser.php
@@ -11,50 +11,37 @@ class CheckGatewayAuthUser
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
 
-        if (config('app.env') === 'production') {
-            $auth_user = $request->input('auth_user');
-            if (!$auth_user) {
+
+        $auth_user = $request->input('auth_user');
+        if (!$auth_user) {
+            throw new BusinessException('Unauthenticated', 401);
+        }
+
+        if (is_string($auth_user)) {
+            $auth_user = json_decode($auth_user, true);
+            if (!$auth_user['prm_admin']) {
                 throw new BusinessException('Unauthenticated', 401);
             }
 
-            if (is_string($auth_user)) {
-                $auth_user = json_decode($auth_user, true);
-                if(!$auth_user['prm_admin']) {
-                    throw new BusinessException('Unauthenticated', 401);
-                }
-
-                if(!array_key_exists('avatar_link', $auth_user)){
-                    $auth_user['avatar_link'] = null;
-                }
-            } else if(is_array($auth_user)) {
-
-                if(!$auth_user['prm_admin']) {
-                    throw new BusinessException('Unauthenticated', 401);
-                }
-
-                if(!array_key_exists('avatar_link', $auth_user)){
-                    $auth_user['avatar_link'] = null;
-                }
+            if (!array_key_exists('avatar_link', $auth_user)) {
+                $auth_user['avatar_link'] = null;
             }
-        } else {
-            $auth_user = [
-                'id' => 1,
-                'name' => 'asd',
-                'phone' => '998998921652',
-                'created_at' => 'sadas',
-                'avatar_link' => null,
-                'prm_admin' => [
-                    'id' => 6,
-                    'user_id' => 6
-                ]
-            ];
+        } else if (is_array($auth_user)) {
+
+            if (!$auth_user['prm_admin']) {
+                throw new BusinessException('Unauthenticated', 401);
+            }
+
+            if (!array_key_exists('avatar_link', $auth_user)) {
+                $auth_user['avatar_link'] = null;
+            }
         }
         $user = new User($auth_user);
         app()->instance(User::class, $user);
