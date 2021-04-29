276c3d337f6e279c6533ab461e7b579f966ff66d
orejametov-dev <orejametov777@gmail.com>
2021-04-29 12:01:46 +0500

    small fix

--- app/Http/Middleware/CheckGatewayAuthUser.php
+++ app/Http/Middleware/CheckGatewayAuthUser.php
@@ -18,15 +18,12 @@ class CheckGatewayAuthUser
     public function handle($request, Closure $next)
     {
         $auth_user = $request->header('x-auth-user');
-
         if (!$auth_user) {
             throw new BusinessException('Unauthenticated', 401);
         }
-
         $auth_user = json_decode($auth_user, true);
-
-        $user = new User($auth_user);
-        app()->instance(User::class, $user);
+        app()->instance(User::class, $auth_user);
+        dd();
         return $next($request);
     }
 }
