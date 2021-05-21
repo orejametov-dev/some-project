4076b752b961d482ba9771ed69b88af616096842
orejametov <orejametov777@gmail.com>
2021-05-21 10:48:17 +0500

    removed unused code

--- app/Http/Middleware/CheckGatewayAuthUser.php
+++ app/Http/Middleware/CheckGatewayAuthUser.php
@@ -19,15 +19,6 @@ class CheckGatewayAuthUser
     {
         $auth_user = $request->header('x-auth-user');
 
-        if(config('app.env') == 'local'){
-            $auth_user = json_encode([
-                'id' => 1,
-                'name' => 'Oybek Rejametov',
-                'phone' => '998998921652',
-                'avatar_link' => null
-            ]);
-        }
-
         if (!$auth_user) {
             throw new BusinessException('Unauthenticated', 401);
         }
