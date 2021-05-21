1be6387f555d2857a761c699d3a78c680fed054a
orejametov <orejametov777@gmail.com>
2021-05-21 10:38:27 +0500

    unused values

--- app/Http/Middleware/CheckGatewayAuthUser.php
+++ app/Http/Middleware/CheckGatewayAuthUser.php
@@ -19,15 +19,6 @@ class CheckGatewayAuthUser
     {
         $auth_user = $request->header('x-auth-user');
 
-        if(config('app.env') != 'production') {
-            $auth_user = json_encode([
-                'id' => 6,
-                'name' => 'Oybek Rejametov',
-                'avatar_link' => null,
-                'phone' => '998998921652'
-            ]);
-        }
-
         if (!$auth_user) {
             throw new BusinessException('Unauthenticated', 401);
         }
