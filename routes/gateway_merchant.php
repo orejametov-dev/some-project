a0de80375add2ff10b3773fc032fb5013ba78835
orejametov <orejametov777@gmail.com>
2021-06-18 08:54:40 +0500

    refactored merchant requests routes

--- routes/gateway_merchant.php
+++ routes/gateway_merchant.php
@@ -21,10 +21,10 @@ Route::prefix('merchants/problem-cases')
 Route::prefix('merchants/requests')
     ->group(function () {
         Route::get('/app', [\App\Http\Controllers\ApiMerchantGateway\Merchants\MerchantRequestsController::class, 'app'])->withoutMiddleware(['gateway-access', 'gateway-auth-user']);
-        Route::get('/{id}', [\App\Http\Controllers\ApiMerchantGateway\Merchants\MerchantRequestsController::class, 'show'])->withoutMiddleware(['gateway-access', 'gateway-auth-user']);
-        Route::post('/', [\App\Http\Controllers\ApiMerchantGateway\Merchants\MerchantRequestsController::class, 'store'])->withoutMiddleware(['gateway-access', 'gateway-auth-user']);
-        Route::put('/{id}', [\App\Http\Controllers\ApiMerchantGateway\Merchants\MerchantRequestsController::class, 'update'])->withoutMiddleware(['gateway-access', 'gateway-auth-user']);
-        Route::post('/{id}/upload-files', [\App\Http\Controllers\ApiMerchantGateway\Merchants\MerchantRequestsController::class, 'upload'])->withoutMiddleware(['gateway-access', 'gateway-auth-user']);
+        Route::get('/{token}', [\App\Http\Controllers\ApiMerchantGateway\Merchants\MerchantRequestsController::class, 'show'])->withoutMiddleware(['gateway-access', 'gateway-auth-user']);
+        Route::post('/store-main', [\App\Http\Controllers\ApiMerchantGateway\Merchants\MerchantRequestsController::class, 'storeMain'])->withoutMiddleware(['gateway-access', 'gateway-auth-user']);
+        Route::post('/store-documents', [\App\Http\Controllers\ApiMerchantGateway\Merchants\MerchantRequestsController::class, 'storeDocuments'])->withoutMiddleware(['gateway-access', 'gateway-auth-user']);
+        Route::post('/upload-files', [\App\Http\Controllers\ApiMerchantGateway\Merchants\MerchantRequestsController::class, 'upload'])->withoutMiddleware(['gateway-access', 'gateway-auth-user']);
     });
 
 Route::prefix('merchants/tags')
