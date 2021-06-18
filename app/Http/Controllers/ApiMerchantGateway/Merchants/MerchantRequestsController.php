19937d4581cc660c5ef38441f88a3756fb05d90d
orejametov <orejametov777@gmail.com>
2021-06-18 09:00:24 +0500

    refactored storing main, documents and upload files of merchant requests

--- app/Http/Controllers/ApiMerchantGateway/Merchants/MerchantRequestsController.php
+++ app/Http/Controllers/ApiMerchantGateway/Merchants/MerchantRequestsController.php
@@ -7,10 +7,9 @@ namespace App\Http\Controllers\ApiMerchantGateway\Merchants;
 use App\Http\Controllers\Controller;
 use App\Modules\Merchants\Models\File;
 use App\Modules\Merchants\Models\Merchant;
+use App\Modules\Merchants\Models\Request as MerchantRequest;
 use App\Services\RegionService;
 use Illuminate\Http\Request;
-use App\Modules\Merchants\Models\Request as MerchantRequest;
-use Illuminate\Support\Str;
 use Illuminate\Validation\Rule;
 
 class MerchantRequestsController extends Controller
@@ -26,9 +25,9 @@ class MerchantRequestsController extends Controller
         ];
     }
 
-    public function show($id) {
-
-        if(preg_match('/^\d+$/', $id)) {
+    public function show($id)
+    {
+        if (preg_match('/^\d+$/', $id)) {
             $merchant_request = MerchantRequest::with('files')->find($id);
         } else {
             $merchant_request = MerchantRequest::with('files')->where('token', $id)->first();
@@ -37,9 +36,10 @@ class MerchantRequestsController extends Controller
         return $merchant_request;
     }
 
-    public function store(Request $request)
+    public function storeMain(Request $request)
     {
         $validatedRequest = $this->validate($request, [
+            'token' => 'required|string',
             'user_name' => 'required|string',
             'user_phone' => 'required|digits:12',
             'name' => 'required|string',
@@ -50,27 +50,18 @@ class MerchantRequestsController extends Controller
             'information' => 'nullable|string',
             'region' => 'required|string',
         ]);
-        $merchant_request = new MerchantRequest($validatedRequest);
-        $merchant_request->token = Str::uuid();
+        $merchant_request = MerchantRequest::updateOrCreate(['token' => $request->input('token')], $validatedRequest);
         $merchant_request->setStatusNew();
         $merchant_request->save();
+        $merchant_request->checkToCompleted();
 
         return $merchant_request;
     }
 
-    public function update($id, Request $request)
+    public function storeDocuments(Request $request)
     {
         $validatedRequest = $this->validate($request, [
-            'user_name' => 'required|string',
-            'user_phone' => 'required|digits:12',
-            'name' => 'required|string',
-            'categories' => 'required|array',
-            'stores_count' => 'required|integer',
-            'merchant_users_count' => 'required|integer',
-            'approximate_sales' => 'required|integer',
-            'information' => 'nullable|string',
-            'region' => 'required|string',
-
+            'token' => 'required|string',
             'director_name' => 'required|max:255',
             'legal_name' => 'required|string',
             'phone' => 'required|digits:12',
@@ -83,16 +74,18 @@ class MerchantRequestsController extends Controller
             'address' => 'required|string'
         ]);
 
-        $merchant_request = MerchantRequest::findOrFail($id);
-        $merchant_request->fill($validatedRequest);
+        $merchant_request = MerchantRequest::updateOrCreate(['token' => $request->input('token')], $validatedRequest);
         $merchant_request->save();
+        $merchant_request->checkToCompleted();
 
         return $merchant_request;
     }
 
-    public function upload($id, Request $request) {
+    public function upload(Request $request)
+    {
 
         $this->validate($request, [
+            'token' => 'request|string',
             'file_type' => [
                 'required',
                 'string',
@@ -103,8 +96,10 @@ class MerchantRequestsController extends Controller
         ]);
 
         /** @var Merchant $merchant */
-        $merchant_request = MerchantRequest::query()->findOrFail($id);
+        $merchant_request = MerchantRequest::query()->where('token', $request->input('token'))->firstOrFail();
         $merchant_request_file = $merchant_request->uploadFile($request->file('file'), $request->input('file_type'));
+        $merchant_request->checkToCompleted();
+
         return $merchant_request_file;
     }
 }
