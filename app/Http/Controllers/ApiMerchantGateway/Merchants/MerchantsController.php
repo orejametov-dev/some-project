db6143cb89635437af6c00f1c9964dfcca1be975
orejametov <orejametov777@gmail.com>
2021-07-10 18:14:10 +0500

    added new route for getting merchant and his relations

--- app/Http/Controllers/ApiMerchantGateway/Merchants/MerchantsController.php
+++ app/Http/Controllers/ApiMerchantGateway/Merchants/MerchantsController.php
@@ -7,6 +7,7 @@ namespace App\Http\Controllers\ApiMerchantGateway\Merchants;
 use App\Http\Controllers\ApiMerchantGateway\ApiBaseController;
 use App\Modules\Merchants\Models\Condition;
 use App\Modules\Merchants\Models\Merchant;
+use App\Modules\Merchants\Models\Store;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 
@@ -19,13 +20,22 @@ class MerchantsController extends ApiBaseController
             return Merchant::findOrFail($this->merchant_id);
         });
 
-        $merchant = Cache::remember('detail_cache_of_merchant_conditions_' . $this->merchant_id, 60 * 60, function () use ($merchant) {
-            return Condition::query()->active()->where('merchant_id', $merchant->id)->get();
+        $conditions = Cache::remember('detail_cache_of_merchant_conditions_' . $this->merchant_id, 60 * 60, function () use ($merchant) {
+            return Condition::query()->active()->byMerchant($merchant->id)->get();
         });
 
+        $stores = Cache::remember('detail_cache_of_merchant_stores_' . $this->merchant_id, 60 * 60, function () use ($merchant) {
+            $stores = Store::query()->byMerchant($merchant->id)->get();
 
-        $stores = $merchant->stores;
+            foreach ($stores as $store) {
+                if($store->id == $this->store_id) {
+                    $store->is_merchant_user_store = true;
+                    $stores->merge($stores);
+                }
+            }
 
+            return $stores;
+        });
 
         return [
             'merchant' => $merchant,
