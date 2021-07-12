23d55c7ffd6dd50a9bf3eb36cfa1b4ad8f590841
dev-iskan <successfuliskan@gmail.com>
2021-07-12 14:12:53 +0500

    health log

--- app/Http/Controllers/ApiComplianceGateway/Merchants/MerchantsController.php
+++ app/Http/Controllers/ApiComplianceGateway/Merchants/MerchantsController.php
@@ -1,27 +1,17 @@
 <?php
 
-
 namespace App\Http\Controllers\ApiComplianceGateway\Merchants;
-
 
 use App\Http\Controllers\ApiComplianceGateway\ApiBaseController;
 use App\Http\Resources\ApiComplianceGateway\Merchants\MerchantsResource;
 use App\Modules\Merchants\Models\Merchant;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Log;
 
 class MerchantsController extends ApiBaseController
 {
     public function index(Request $request)
     {
-        Log::channel('urgent')->debug('start_service', [
-            'time' => Carbon::createFromTimestamp(LARAVEL_START)
-        ]);
-        Log::channel('urgent')->debug('start_merchant_controller', [
-            'time' => Carbon::now()
-        ]);
         $merchants = Merchant::query()
             ->filterRequest($request)
             ->orderRequest($request);
@@ -29,10 +19,6 @@ class MerchantsController extends ApiBaseController
         $result = Cache::remember($request->fullUrl(), 180, function () use ($merchants, $request) {
             return MerchantsResource::collection($merchants->paginate($request->query('per_page')) ?? 15);
         });
-
-        Log::channel('urgent')->debug('end_merchant_controller', [
-            'time' => Carbon::now()
-        ]);
 
         return $result;
     }
--- app/Http/Controllers/Monitoring/MonitoringController.php
+++ app/Http/Controllers/Monitoring/MonitoringController.php
@@ -4,14 +4,26 @@ namespace App\Http\Controllers\Monitoring;
 
 use App\Http\Controllers\Controller;
 use App\Modules\Merchants\Models\Merchant;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
 
 class MonitoringController extends Controller
 {
     public function general()
     {
+        Log::channel('urgent')->debug('start_service', [
+            'time' => Carbon::createFromTimestamp(LARAVEL_START)
+        ]);
+        Log::channel('urgent')->debug('start_controller', [
+            'time' => Carbon::now()
+        ]);
+
         //check database
         Merchant::query()->limit(1)->get();
 
+        Log::channel('urgent')->debug('end_controller', [
+            'time' => Carbon::now()
+        ]);
         return response()->json(['status' => 'OK']);
     }
 }
