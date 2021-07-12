70ae36f112ecff5fc6c5763d09b977fd773f2075
ramzesh <ramzesh.halifionakis@gmail.com>
2021-07-12 14:11:17 +0500

    logs

---
+++ database/migrations/2021_07_12_085904_create_table_logs.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableLogs extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('logs')->create('logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('started_at');
+            $table->unsignedBigInteger('finished_at');
+            $table->unsignedBigInteger('diff');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection('logs')->dropIfExists('logs');
+    }
+}
