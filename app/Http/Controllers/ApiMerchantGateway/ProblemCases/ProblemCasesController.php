b175255cd717197647a7dd84161950852edf48bd
orejametov <orejametov777@gmail.com>
2021-07-13 12:06:36 +0500

    smallfix

--- app/Http/Controllers/ApiMerchantGateway/ProblemCases/ProblemCasesController.php
+++ app/Http/Controllers/ApiMerchantGateway/ProblemCases/ProblemCasesController.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\ApiMerchantGateway\ApiBaseController;
 use App\Http\Resources\ApiMerchantGateway\ProblemCases\ProblemCaseResource;
 use App\Modules\Merchants\Models\ProblemCase;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Cache;
 
 class ProblemCasesController extends ApiBaseController
@@ -60,7 +61,7 @@ class ProblemCasesController extends ApiBaseController
 
         $problemCase->save();
 
-        return $problemCase;
+        return new ProblemCaseResource($problemCase);
     }
 
     public function setEngage($id)
