d4333590d71ac79aed2df36fc1c804ee2160443e
orejametov <orejametov777@gmail.com>
2021-07-14 10:01:58 +0500

    fixed cache struct of special merchant route

--- app/Http/Controllers/ApiMerchantGateway/Merchants/MerchantsController.php
+++ app/Http/Controllers/ApiMerchantGateway/Merchants/MerchantsController.php
@@ -16,31 +16,28 @@ class MerchantsController extends ApiBaseController
     //роут для фронт мерчанта
     public function getMerchantDetailsWithRelations(Request $request)
     {
-        $merchant = Cache::remember('detail_cache_of_merchant_' . $this->merchant_id, 60 * 60, function () {
+        $merchant = Cache::tags($this->merchant_id)->remember('cache_of_merchant', 60 * 60, function () {
             return Merchant::findOrFail($this->merchant_id);
         });
 
-        $conditions = Cache::remember('detail_cache_of_merchant_conditions_' . $this->merchant_id, 60 * 60, function () use ($merchant) {
+        $conditions = Cache::tags($this->merchant_id)->remember('cache_of_merchant_conditions', 60 * 60, function () use ($merchant) {
             return Condition::query()->active()->byMerchant($merchant->id)->get();
         });
 
-        $stores = Cache::remember('detail_cache_of_merchant_stores_' . $this->merchant_id, 60 * 60, function () use ($merchant) {
-            $stores = Store::query()->byMerchant($merchant->id)->get();
+        $stores = Cache::tags($this->merchant_id)->remember('cache_of_merchant_stores', 60 * 60, function () use ($merchant) {
+            return Store::query()->byMerchant($merchant->id)->get();
+        });
 
-            foreach ($stores as $store) {
-                if($store->id == $this->store_id) {
-                    $store->is_merchant_user_store = true;
-                    $stores->merge($stores);
-                }
-            }
-
-            return $stores;
+        $merchant_user = $this->user->merchant_user;
+        $store = Cache::tags($this->merchant_id)->remember($merchant_user->id.'detail_cache_of_merchant_stores', 60 * 60, function () use ($merchant_user) {
+            return Store::query()->findOrFail($merchant_user->store_id);
         });
 
         return [
             'merchant' => $merchant,
             'conditions' => $conditions,
-            'stores' => $stores
+            'stores' => $stores,
+            'store' => $store
         ];
     }
 }
