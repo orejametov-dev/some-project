2390af9d535ebb3e74d7cbcb5a49778370ed029b
orejametov <orejametov777@gmail.com>
2021-07-14 10:21:30 +0500

    small fix

--- app/Http/Controllers/ApiMerchantGateway/Merchants/MerchantsController.php
+++ app/Http/Controllers/ApiMerchantGateway/Merchants/MerchantsController.php
@@ -16,22 +16,38 @@ class MerchantsController extends ApiBaseController
     //роут для фронт мерчанта
     public function getMerchantDetailsWithRelations(Request $request)
     {
-        $merchant = Cache::tags($this->merchant_id)->remember('cache_of_merchant', 60 * 60, function () {
+        $merchant = Cache::remember($this->merchant_id.'cache_of_merchant', 60 * 60, function () {
             return Merchant::findOrFail($this->merchant_id);
         });
 
-        $conditions = Cache::tags($this->merchant_id)->remember('cache_of_merchant_conditions', 60 * 60, function () use ($merchant) {
+//        $merchant = Cache::tags($this->merchant_id)->remember('cache_of_merchant', 60 * 60, function () {
+//            return Merchant::findOrFail($this->merchant_id);
+//        });
+
+        $conditions = Cache::remember($this->merchant_id.'cache_of_merchant_conditions', 60 * 60, function () use ($merchant) {
             return Condition::query()->active()->byMerchant($merchant->id)->get();
         });
 
-        $stores = Cache::tags($this->merchant_id)->remember('cache_of_merchant_stores', 60 * 60, function () use ($merchant) {
+//        $conditions = Cache::tags($this->merchant_id)->remember('cache_of_merchant_conditions', 60 * 60, function () use ($merchant) {
+//            return Condition::query()->active()->byMerchant($merchant->id)->get();
+//        });
+
+        $stores = Cache::remember($this->merchant_id.'cache_of_merchant_stores', 60 * 60, function () use ($merchant) {
             return Store::query()->byMerchant($merchant->id)->get();
         });
 
+//        $stores = Cache::tags($this->merchant_id)->remember('cache_of_merchant_stores', 60 * 60, function () use ($merchant) {
+//            return Store::query()->byMerchant($merchant->id)->get();
+//        });
+
         $merchant_user = $this->user->merchant_user;
-        $store = Cache::tags($this->merchant_id)->remember($merchant_user->id.'detail_cache_of_merchant_stores', 60 * 60, function () use ($merchant_user) {
+        $store = Cache::remember($this->merchant_id.$merchant_user->id.'detail_cache_of_merchant_stores', 60 * 60, function () use ($merchant_user) {
             return Store::query()->findOrFail($merchant_user->store_id);
         });
+
+//        $store = Cache::tags($this->merchant_id)->remember($merchant_user->id.'detail_cache_of_merchant_stores', 60 * 60, function () use ($merchant_user) {
+//            return Store::query()->findOrFail($merchant_user->store_id);
+//        });
 
         return [
             'merchant' => $merchant,
