00a25ec1d90fca19a54fbcde6f2294c97ef8b995
orejametov <orejametov777@gmail.com>
2021-07-16 17:06:17 +0500

    created table archive_reasons

---
+++ app/Modules/Merchants/Models/ArchiveReason.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Modules\Merchants\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ArchiveReason extends Model
+{
+    use HasFactory;
+}
