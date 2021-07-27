104d953476f73354797fd0171fbb959c69479989
orejametov <orejametov777@gmail.com>
2021-07-27 15:47:06 +0500

    small fix

--- app/Http/Controllers/ApiMerchantGateway/ApiBaseController.php
+++ app/Http/Controllers/ApiMerchantGateway/ApiBaseController.php
@@ -19,11 +19,11 @@ class ApiBaseController extends Controller
     {
         $this->middleware(function ($request, $next) {
             $this->user = app(User::class);
-            $merchant_user = Cache::tags('merchants')->remember('merchant_user_id_' . $this->user->id, 86400, function () use ($user_id) {
+            $merchant_user = Cache::tags('merchants')->remember('merchant_user_id_' . $this->user->id, 86400, function () {
                 $merchant_user = MerchantUser::query()->with(['merchant', 'store'])
                     ->byActiveMerchant()
                     ->byActiveStore()
-                    ->byUserId($user_id)->first();
+                    ->byUserId($this->user->id)->first();
                 return $merchant_user;
             });
             $this->merchant_id = $merchant_user->merchant_id;
