4b32471d49c0f02517c2b48075e0733de366a66b
orejametov <orejametov777@gmail.com>
2021-08-05 15:52:29 +0500

    small fix

--- app/Http/Requests/ApiPrm/Merchants/StoreMerchantInfo.php
+++ app/Http/Requests/ApiPrm/Merchants/StoreMerchantInfo.php
@@ -34,7 +34,6 @@ class StoreMerchantInfo extends FormRequest
             'address' => 'required',
             'bank_account' => 'required|digits:20',
             'bank_name' => 'required|max:255',
-            'limit' => 'required'
         ];
     }
 }
