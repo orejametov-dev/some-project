1220e810de545e5a1090774df4f06406f8552f93
orejametov <orejametov777@gmail.com>
2021-08-13 09:46:06 +0500

    removed permissions

---
+++ database/migrations/2021_08_13_044052_remove_permissions_of_merchant_users.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RemovePermissionsOfMerchantUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('merchant_users', function (Blueprint $table) {
+            $table->dropColumn('permission_applications');
+            $table->dropColumn('permission_deliveries');
+            $table->dropColumn('permission_manager');
+            $table->dropColumn('permission_upload_goods');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('merchant_users', function (Blueprint $table) {
+            $table->boolean('permission_applications')->default(false);
+            $table->boolean('permission_deliveries')->default(false);
+            $table->boolean('permission_manager')->default(false);
+            $table->boolean('permission_upload_goods')->default(false);
+        });
+    }
+}
