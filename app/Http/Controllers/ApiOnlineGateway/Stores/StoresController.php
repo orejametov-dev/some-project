3498e00739a3be90bfeeb73c10079ea046ce13a2
orejametov <orejametov777@gmail.com>
2021-09-14 14:15:03 +0500

    added store resource and controller

---
+++ app/Http/Controllers/ApiOnlineGateway/Stores/StoresController.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Http\Controllers\ApiOnlineGateway\Stores;
+
+
+use App\Http\Controllers\Controller;
+use App\Modules\Merchants\Models\Store;
+use Illuminate\Http\Request;
+
+class StoresController extends Controller
+{
+    public function index(Request $request)
+    {
+        $stores = Store::query()
+            ->active()
+            ->filterRequest($request);
+
+        return $stores->paginate($request->query('per_page'));
+    }
+}
