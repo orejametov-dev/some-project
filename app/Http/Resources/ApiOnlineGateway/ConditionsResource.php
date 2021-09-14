9de443328c852e87d33ca36861aa24339eef07e8
orejametov <orejametov777@gmail.com>
2021-09-14 14:01:49 +0500

    added conditions resource in online apis

---
+++ app/Http/Resources/ApiOnlineGateway/ConditionsResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources\ApiOnlineGateway;
+
+use App\Modules\Merchants\Models\Merchant;
+use App\Modules\Merchants\Models\Store;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ConditionsResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param Request $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        /** @var Store|StoresResource $this */
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'address' => $this->address,
+        ];
+    }
+}
