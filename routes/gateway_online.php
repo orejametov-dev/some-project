c72bb8fdea02d5174bd116ca8e35423c8d351cae
orejametov <orejametov777@gmail.com>
2021-09-14 14:02:28 +0500

    added public apis

--- routes/gateway_online.php
+++ routes/gateway_online.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ApiOnlineGateway\Merchants\MerchantsController as OnlineMerchantsController;
+use App\Http\Controllers\ApiOnlineGateway\Stores\StoresController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -8,5 +9,32 @@ Route::prefix('merchants')
     ->group(function () {
         Route::get('/', [OnlineMerchantsController::class, 'index']);
         Route::get('tags', [OnlineMerchantsController::class, 'tags']);
+    });
+
+Route::prefix('public')
+    ->group(function () {
+        Route::prefix('merchants')
+            ->group(function () {
+                Route::get('/', [OnlineMerchantsController::class, 'index'])
+                    ->withoutMiddleware(['gateway-auth-user']);
+
+                Route::get('tags', [OnlineMerchantsController::class, 'tags'])
+                    ->withoutMiddleware(['gateway-auth-user']);
+
+                Route::get('/{id}', [OnlineMerchantsController::class, 'show'])
+                    ->withoutMiddleware(['gateway-auth-user']);
+            });
+
+        Route::prefix('stores')
+            ->group(function () {
+                Route::get('/', [StoresController::class, 'index'])
+                    ->withoutMiddleware(['gateway-auth-user']);
+            });
+
+        Route::prefix('conditions')
+            ->group(function () {
+                Route::get('/', [StoresController::class, 'index'])
+                    ->withoutMiddleware(['gateway-auth-user']);
+            });
     });
 
