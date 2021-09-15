002dc0db0fb43671340e214952c16579db0f7d42
ArtKain <akaxiro@gmail.com>
2021-09-15 18:20:35 +0500

    add new command CoreService2.php

---
+++ app/HttpServices/Core/CoreService2.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\HttpServices\Core;
+
+use Illuminate\Support\Facades\Http;
+
+class CoreService2
+{
+    public function getMerchantApplicationsAndClientsCountByRange($merchant_id, $from_date, $to_date)
+    {
+        return static::http()->get('merchant-activity' , [
+            'from_date' => $from_date,
+            'to_date' => $to_date,
+            'merchant_id' => $merchant_id,
+        ])->json();
+    }
+
+    public function getStoreApplicationsAndClientsCountByRange($store_id, $from_date, $to_date)
+    {
+        return static::http()->get('merchant-activity' , [
+            'from_date' => $from_date,
+            'to_date' => $to_date,
+            'store_id' => $store_id,
+        ])->json();
+    }
+
+    protected static function http()
+    {
+        return Http::baseUrl(config('local_services.service_core.domain') . '/')
+            ->withHeaders([
+                'Accept' => 'application/json',
+                'Access-Token' => config('local_service.service_core.service_token'),
+                'Content-Type' => 'application/json'
+            ]);
+    }
+}
