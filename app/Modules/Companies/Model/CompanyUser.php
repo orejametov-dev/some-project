76a7a102455c5c8091515ace3c4779b366be64cc
orejametov <orejametov777@gmail.com>
2021-09-21 15:53:41 +0500

    create CompanyUser Model

---
+++ app/Modules/Companies/Model/CompanyUser.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Modules\Companies\Model;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CompanyUser extends Model
+{
+    use HasFactory;
+}
