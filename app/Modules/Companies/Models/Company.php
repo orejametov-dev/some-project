b4d1034518e47332a4c68b793839267d3ffc3b19
orejametov <orejametov777@gmail.com>
2021-09-22 16:51:49 +0500

    added company_users relation

--- app/Modules/Companies/Models/Company.php
+++ app/Modules/Companies/Models/Company.php
@@ -1,20 +1,29 @@
 <?php
 
-namespace App\Modules\Companies\Model;
+namespace App\Modules\Companies\Models;
 
 use App\Modules\Merchants\Models\Merchant;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property string $name
+ * @property string|null $legal_name
+ */
 class Company extends Model
 {
     use HasFactory;
+
     protected $fillable = ['name', 'legal_name'];
+
+    public function company_users()
+    {
+        return $this->hasMany(CompanyUser::class);
+    }
 
     public function merchant()
     {
         return $this->hasOne(Merchant::class);
     }
-
-//    public function
 }
