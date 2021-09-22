7cdf136aab657458020f7e1091b1f9773055e966
Artyom Kuzminskiy <@artyom_k>
2021-09-22 17:45:55 +0500

    add new columns application_created_at and credit_contract_date to problem cases table

---
+++ database/migrations/2021_09_22_071643_add_new_application_created_at_and_credit_contract_date_columns_to_problem_cases_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNewApplicationCreatedAtAndCreditContractDateColumnsToProblemCasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('problem_cases', function (Blueprint $table) {
+            $table->date('application_created_at')->nullable();
+            $table->date('credit_contract_date')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('problem_cases', function (Blueprint $table) {
+            //
+        });
+    }
+}
