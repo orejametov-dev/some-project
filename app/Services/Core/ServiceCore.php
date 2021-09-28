02fd1339fc5807439651db58062d80fb5fb1afca
orejametov <orejametov777@gmail.com>
2021-09-28 20:34:38 +0500

    small fix

--- app/Services/Core/ServiceCore.php
+++ app/Services/Core/ServiceCore.php
@@ -7,6 +7,7 @@ use App\Modules\Core\Models\WebService;
 use GuzzleHttp\Client as HttpClient;
 use GuzzleHttp\ClientInterface;
 use GuzzleHttp\Exception\RequestException;
+use Illuminate\Support\Facades\Log;
 use Psr\Http\Message\ResponseInterface;
 
 class ServiceCore
@@ -39,6 +40,7 @@ class ServiceCore
                 ],
                 $key => $params
             ]);
+            Log::info($response->getBody()->getContents());
             if (!$should_return_response) {
                 return self::parseResponse($response);
             } else {
