2c739fc45fd1b1810b04d21d0942483e07f13088
orejametov <orejametov777@gmail.com>
2021-09-29 11:53:42 +0500

    added filter to Company model

--- app/Modules/Companies/Models/Company.php
+++ app/Modules/Companies/Models/Company.php
@@ -3,13 +3,18 @@
 namespace App\Modules\Companies\Models;
 
 use App\Modules\Merchants\Models\Merchant;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 /**
  * @property int $id
  * @property string $name
  * @property string|null $legal_name
+ * @method static Builder|Merchant filterRequest(Request $request)
+
  */
 class Company extends Model
 {
@@ -25,5 +30,19 @@ class Company extends Model
     public function merchant()
     {
         return $this->hasOne(Merchant::class);
+    }
+
+    public function scopeFilterRequest(Builder $query, Request $request)
+    {
+        if ($q = $request->query('q')) {
+            $query->where('name', 'like', '%' . $q . '%')
+                ->orWhere('legal_name', 'like', '%' . $q . '%');
+
+        }
+
+        if ($request->query('date')) {
+            $date = Carbon::parse($request->query('date'));
+            $query->whereDate('created_at', $date);
+        }
     }
 }
