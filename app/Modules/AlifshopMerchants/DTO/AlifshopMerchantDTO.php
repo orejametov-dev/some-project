5153a4a268c3108f7abdc6346e3d1a0e71ef61c5
Artyom Kuzminskiy <@artyom_k>
2021-10-05 17:19:10 +0500

    create AlifshopMerchantDTO.php

---
+++ app/Modules/AlifshopMerchants/DTO/AlifshopMerchantDTO.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Modules\AlifshopMerchants\DTO;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class AlifshopMerchantDTO
+{
+//    public int company_id;
+//    public int mait
+}
