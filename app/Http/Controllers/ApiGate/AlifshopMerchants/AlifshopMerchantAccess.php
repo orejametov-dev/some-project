e20ec2a691dc798532d0a87306e73743a2cc217e
Artyom Kuzminskiy <@artyom_k>
2021-10-05 15:15:10 +0500

    add new model and route

---
+++ app/Http/Controllers/ApiGate/AlifshopMerchants/AlifshopMerchantAccess.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\ApiGate\AlifshopMerchants;
+
+use App\Http\Controllers\Controller;
+use App\Modules\AlifshopMerchants\AlifshopMerchant;
+use Illuminate\Support\Facades\Cache;
+
+class AlifshopMerchantAccess extends Controller
+{
+  //
+}
---
+++ app/Http/Controllers/ApiGateway/ApiAlifshopMerchants/AlifshopStores/AlifshopMerchantStoresController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\ApiGateway\ApiAlifshopMerchants\AlifshopStores;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AlifshopMerchantStoresController extends Controller
+{
+    //
+}
