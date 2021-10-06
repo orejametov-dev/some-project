147b8d1ef824066a8bdd896e1fa402d2c675248d
Artyom Kuzminskiy <@artyom_k>
2021-10-06 16:51:40 +0500

    add @property $id

--- app/Modules/AlifshopMerchants/Models/AlifshopMerchantStores.php
+++ app/Modules/AlifshopMerchants/Models/AlifshopMerchantStores.php
@@ -3,10 +3,12 @@
 namespace App\Modules\AlifshopMerchants\Models;
 
 use App\Modules\AlifshopMerchant\Traits\AlifshopMerchantStoreRelationshipsTrait;
+use App\Modules\Companies\Models\Company;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
+ * @property $id
  * @property AlifshopMerchant $alifshopMerchant
  */
 class AlifshopMerchantStores extends Model
