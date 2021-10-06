b9d58f73396c624f193a8ac4be34856a2b59923b
Artyom Kuzminskiy <@artyom_k>
2021-10-06 13:52:01 +0500

    add Trait

--- app/Modules/AlifshopMerchants/Models/AlifshopMerchantStores.php
+++ app/Modules/AlifshopMerchants/Models/AlifshopMerchantStores.php
@@ -2,10 +2,15 @@
 
 namespace App\Modules\AlifshopMerchants\Models;
 
+use App\Modules\AlifshopMerchant\Traits\AlifshopMerchantStoreRelationshipsTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property AlifshopMerchant $alifshopMerchant
+ */
 class AlifshopMerchantStores extends Model
 {
     use HasFactory;
+    use AlifshopMerchantStoreRelationshipsTrait;
 }
