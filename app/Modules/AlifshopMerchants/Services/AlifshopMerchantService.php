1b78dec3d844d7481ba5d84801814cb1336d540d
Artyom Kuzminskiy <@artyom_k>
2021-10-06 13:51:35 +0500

    add new parameter from create()

--- app/Modules/AlifshopMerchants/Services/AlifshopMerchantService.php
+++ app/Modules/AlifshopMerchants/Services/AlifshopMerchantService.php
@@ -10,6 +10,12 @@ class AlifshopMerchantService
     public function create(AlifshopMerchantDTO $alifshopMerchantDTO)
     {
         $alifshop_merchant =  new AlifshopMerchant();
+        $alifshop_merchant->id = $alifshopMerchantDTO->id;
+        $alifshop_merchant->name = $alifshopMerchantDTO->name;
+        $alifshop_merchant->legal_name = $alifshopMerchantDTO->legal_name;
+        $alifshop_merchant->token = $alifshopMerchantDTO->token;
+        $alifshop_merchant->alifshop_slug = $alifshopMerchantDTO->alifshop_slug;
+        $alifshop_merchant->information = $alifshopMerchantDTO->information;
         $alifshop_merchant->maintainer_id = $alifshopMerchantDTO->maintainer_id;
         $alifshop_merchant->company_id = $alifshopMerchantDTO->company_id;
         $alifshop_merchant->save();
