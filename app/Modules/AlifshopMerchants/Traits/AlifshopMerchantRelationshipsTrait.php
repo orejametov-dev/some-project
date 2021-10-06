b50f4eae83c87a78596bc6ee9400b8e05d49fb20
Artyom Kuzminskiy <@artyom_k>
2021-10-06 16:50:17 +0500

    add new relationship

--- app/Modules/AlifshopMerchants/Traits/AlifshopMerchantRelationshipsTrait.php
+++ app/Modules/AlifshopMerchants/Traits/AlifshopMerchantRelationshipsTrait.php
@@ -3,7 +3,18 @@
 namespace App\Modules\AlifshopMerchant\Traits;
 
 
+use App\Modules\AlifshopMerchants\Models\AlifshopMerchantStores;
+use App\Modules\Companies\Models\Company;
+
 trait AlifshopMerchantRelationshipsTrait
 {
-    //
+    public function alifshop_merchant_stores()
+    {
+        return $this->hasMany(AlifshopMerchantStores::class);
+    }
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
 }
