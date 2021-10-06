cd53bb6729d9c8403830d29b0b20fe09065b2e9d
Artyom Kuzminskiy <@artyom_k>
2021-10-06 13:47:35 +0500

    add builder

--- app/Modules/AlifshopMerchants/Models/AlifshopMerchantAccess.php
+++ app/Modules/AlifshopMerchants/Models/AlifshopMerchantAccess.php
@@ -2,9 +2,16 @@
 
 namespace App\Modules\AlifshopMerchants\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
+/**
+ * @method static Builder|AlifshopMerchantAccess filterRequest(Request $request)
+ * @method static Builder|AlifshopMerchantAccess orderRequest(Request $request, string $default_order_str = 'id:desc')
+ * @method static Builder|AlifshopMerchantAccess query()
+ */
 class AlifshopMerchantAccess extends Model
 {
     use HasFactory;
