c2fb78e5b0921bb756cb6bce06447eca1d0fcba8
Artyom Kuzminskiy <@artyom_k>
2021-10-06 13:44:49 +0500

    add new parameters in AlifshopMerchantDTO.php

--- database/migrations/2021_10_05_060455_create_alifshop_merchants_table.php
+++ database/migrations/2021_10_05_060455_create_alifshop_merchants_table.php
@@ -16,6 +16,11 @@ class CreateAlifshopMerchantsTable extends Migration
         Schema::create('alifshop_merchants', function (Blueprint $table) {
             $table->id();
             $table->foreignId('company_id')->constrained('companies');
+            $table->string('name')->unique();
+            $table->string('legal_name')->nullable();
+            $table->string('information')->nullable();
+            $table->string('token')->nullable();
+            $table->string('alifshop_slug');
             $table->boolean('active')->default(false);
             $table->unsignedBigInteger('maintainer_id')->nullable();
             $table->timestamps();
