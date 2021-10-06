b182a626c397f14cadadd7a0061df0df1e2ffd8a
Artyom Kuzminskiy <@artyom_k>
2021-10-06 16:48:47 +0500

    add new @property , use SoftDeletes, $fillable , relationship and FilterRequest

--- app/Modules/AlifshopMerchants/Models/AlifshopMerchantAccess.php
+++ app/Modules/AlifshopMerchants/Models/AlifshopMerchantAccess.php
@@ -2,12 +2,21 @@
 
 namespace App\Modules\AlifshopMerchants\Models;
 
+use App\Modules\Companies\Models\Company;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Http\Request;
 
 /**
+ * @property int $store_id
+ * @property int $user_id
+ * @property string $user_name
+ * @property string $phone
+ * @property-read AlifshopMerchant $alifshopMerchant
+ * @property-read AlifshopMerchantStores $alifshopMerchantStores
  * @method static Builder|AlifshopMerchantAccess filterRequest(Request $request)
  * @method static Builder|AlifshopMerchantAccess orderRequest(Request $request, string $default_order_str = 'id:desc')
  * @method static Builder|AlifshopMerchantAccess query()
@@ -15,4 +24,49 @@ use Illuminate\Http\Request;
 class AlifshopMerchantAccess extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+    protected $fillable = [
+        'user_name',
+        'phone'
+    ];
+
+    public function alifshop_merchant()
+    {
+        return $this->belongsTo(AlifshopMerchant::class);
+    }
+
+    public function alifshop_merchant_store()
+    {
+        return $this->belongsTo(AlifshopMerchantStores::class);
+    }
+
+    public function scopeFilterRequest(Builder $query, Request $request)
+    {
+        if ($q = $request->query('q')) {
+            $query->where('user_name', 'LIKE', '%' . $q . '%')
+                ->orWhere('phone', 'LIKE', '%' . $q . '%');
+        }
+
+        if ($request->query('date')) {
+            $date = Carbon::parse($request->query('date') ?? today());
+            $query->whereDate('created_at', $date);
+        }
+
+        if ($merchant = $request->query('merchant_id')) {
+            $query->where('merchant_id', $merchant);
+        }
+
+        if ($store = $request->query('store_id')) {
+            $query->where('store_id', $store);
+        }
+
+        if ($user = $request->query('user_id')) {
+            $query->where('user_id', $user);
+        }
+
+        if ($user_ids = $request->query('user_ids')) {
+            $user_ids = explode(';', $user_ids);
+            $query->whereIn('user_id', $user_ids);
+        }
+    }
 }
