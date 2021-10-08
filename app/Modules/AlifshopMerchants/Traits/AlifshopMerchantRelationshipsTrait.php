51290462df379a2664cc7ba7e6c43158091d8f2c
Artyom Kuzminskiy <@artyom_k>
2021-10-08 08:56:04 +0500

    add @return

--- app/Modules/AlifshopMerchants/Traits/AlifshopMerchantRelationshipsTrait.php
+++ app/Modules/AlifshopMerchants/Traits/AlifshopMerchantRelationshipsTrait.php
@@ -8,11 +8,18 @@ use App\Modules\Companies\Models\Company;
 
 trait AlifshopMerchantRelationshipsTrait
 {
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     *
+     */
     public function alifshop_merchant_stores()
     {
         return $this->hasMany(AlifshopMerchantStores::class);
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function company()
     {
         return $this->belongsTo(Company::class);
