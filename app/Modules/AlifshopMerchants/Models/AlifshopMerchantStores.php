ccf6830938c17c62eb9d663627ff85af6e17b9e1
Artyom Kuzminskiy <@artyom_k>
2021-10-08 08:57:48 +0500

    rename folder AlifshopMerchant > AlifshopMerchants

--- app/Modules/AlifshopMerchants/Models/AlifshopMerchantStores.php
+++ app/Modules/AlifshopMerchants/Models/AlifshopMerchantStores.php
@@ -2,7 +2,7 @@
 
 namespace App\Modules\AlifshopMerchants\Models;
 
-use App\Modules\AlifshopMerchant\Traits\AlifshopMerchantStoreRelationshipsTrait;
+use App\Modules\AlifshopMerchants\Traits\AlifshopMerchantStoreRelationshipsTrait;
 use App\Modules\Companies\Models\Company;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
