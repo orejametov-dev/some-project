63d0a7ac01f25139916040ab12333f1347547c2f
Artyom Kuzminskiy <@artyom_k>
2021-10-09 16:13:07 +0500

    rename StoreAlifshopMerchantUsers.php > StoreAlifshopMerchantAccess.php

--- app/Http/Requests/ApiPrm/AlifshopMerchant/StoreAlifshopMerchantAccess.php
+++ app/Http/Requests/ApiPrm/AlifshopMerchant/StoreAlifshopMerchantAccess.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\ApiPrm\AlifshopMerchant;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreAlifshopMerchantUsers extends FormRequest
+class StoreAlifshopMerchantAccess extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
