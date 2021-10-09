df23df3cb180f44ff325756886d4c62050528bee
Artyom Kuzminskiy <@artyom_k>
2021-10-09 16:11:48 +0500

    add logic from AlifshopMerchantStoresController.php

--- app/Http/Controllers/ApiGateway/ApiAlifshopMerchants/AlifshopStores/AlifshopMerchantStoresController.php
+++ app/Http/Controllers/ApiGateway/ApiAlifshopMerchants/AlifshopStores/AlifshopMerchantStoresController.php
@@ -2,10 +2,81 @@
 
 namespace App\Http\Controllers\ApiGateway\ApiAlifshopMerchants\AlifshopStores;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\ApiGateway\ApiBaseController;
+use App\Http\Requests\ApiPrm\AlifshopMerchant\AlifshopMerchantStoreStoresRequest;
+use App\Http\Requests\ApiPrm\AlifshopMerchant\AlishopMerchantUpdateStoreRequest;
+use App\Modules\AlifshopMerchants\Models\AlifshopMerchant;
+use App\Modules\AlifshopMerchants\Models\AlifshopMerchantStores;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
-class AlifshopMerchantStoresController extends Controller
+class AlifshopMerchantStoresController extends ApiBaseController
 {
-    //
+    public function index(Request $request)
+    {
+        $alifshop_merchant_stores = AlifshopMerchantStores::query()
+            ->with(['alifshop_merchant:id,company_id,name,legal_name'])
+            ->filterRequest($request);
+
+        return $alifshop_merchant_stores->paginate($request->query('per_page') ?? 15);
+    }
+
+    public  function show($store_id)
+    {
+        $alifshop_merchant_store = AlifshopMerchantStores::query()
+            ->with(['alifshop_merchant:id,company_id,name,legal_name'])
+            ->findOrFail($store_id);
+        return $alifshop_merchant_store;
+    }
+
+    public function store(AlifshopMerchantStoreStoresRequest $request)
+    {
+        $alifshop_merchant = AlifshopMerchant::findOrFail($request->merchant_id);
+
+        if ($alifshop_merchant->alifshop_merchant_stores()->count()) {
+            $alifshop_merchant_store = new AlifshopMerchantStores($request->validated());
+
+            $alifshop_merchant_store->alifshop_merchant_id = $alifshop_merchant->id;
+            $alifshop_merchant_store->save();
+
+            return $alifshop_merchant_store;
+        }
+
+        $alifshop_merchant_store = $alifshop_merchant->alifshop_merchant_stores()
+            ->create(array_merge($request->validated(), ['is_main' => true]));
+
+        Cache::tags($alifshop_merchant->id)->flush();
+        Cache::tags('alifshop_merchants')->flush();
+
+        return $alifshop_merchant_store;
+    }
+
+    public function update(AlishopMerchantUpdateStoreRequest $request, $store_id)
+    {
+        $alifshop_merchant_store = AlifshopMerchantStores::query()->findOrFail($store_id);
+
+        $alifshop_merchant_store->fill($request->validated());
+        $alifshop_merchant_store->save();
+
+        Cache::tags($alifshop_merchant_store->alifshop_merchant_id)->flush();
+        Cache::tags('alifshop_merchants')->flush();
+
+        return $alifshop_merchant_store;
+    }
+
+    public function toggle($id, Request $request)
+    {
+        $this->validate($request, [
+            'active' => 'required|boolean'
+        ]);
+
+        $alifshop_merchant_store = AlifshopMerchantStores::query()->findOrFail($id);
+        $alifshop_merchant_store->active = !$alifshop_merchant_store->active;
+        $alifshop_merchant_store->save();
+
+        Cache::tags($alifshop_merchant_store->alifshop_merchant_id)->flush();
+        Cache::tags('alifshop_merchants')->flush();
+
+        return $alifshop_merchant_store;
+    }
 }
