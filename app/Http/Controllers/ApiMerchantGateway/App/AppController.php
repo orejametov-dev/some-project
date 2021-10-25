439c6b96bb160421cd96d5236b4496ff38e4ec2f
orejametov <orejametov777@gmail.com>
2021-10-25 10:35:50 +0500

    implemented app route for merchant

--- app/Http/Controllers/ApiMerchantGateway/App/AppController.php
+++ app/Http/Controllers/ApiMerchantGateway/App/AppController.php
@@ -5,17 +5,33 @@ namespace App\Http\Controllers\ApiMerchantGateway\App;
 
 
 use App\Http\Controllers\Controller;
+use App\Modules\Companies\Models\CompanyUser;
 use App\Modules\Companies\Models\Module;
-use Illuminate\Http\Request;
+use App\Services\User;
+use Illuminate\Support\Facades\Cache;
 
 class AppController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
         $modules = Module::allCached();
 
+        $company_user_modules = Cache::tags('company')->remember('company_user_' . app(User::class)->id, 24 * 60, function () {
+            $company_user = CompanyUser::query()
+                ->byUser(app(User::class)->id)->first();
+
+            $company_user_modules = [];
+
+            $company_user_modules['merchant_module'] = $company_user->company->merchant()->active()->exists();
+            $company_user_modules['alifshop_module'] = $company_user->company->alifshop_merchant()->active()->exists();
+
+            return $company_user_modules;
+        });
+
+
         return [
-            'modules' => $modules
+            'modules' => $modules,
+            'company_user_modules' => $company_user_modules
         ];
     }
 }
