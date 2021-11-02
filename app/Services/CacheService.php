22a4a0679c7ebb8eb76d2a5b64336c83e5ce62ec
Artyom Kuzminskiy <akaxiro@gmail.ru>
2021-11-02 11:27:37 +0500

    add new Service CacheService,OtpProtector and SmsMessages

---
+++ app/Services/CacheService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Services;
+
+class CacheService
+{
+    public const DOWN_PAYMENTS = 'down_payments';
+    public const OTP = 'otp';
+    public const COMMON = 'common';
+    public const CB_SCORINGS = 'cb_scorings';
+    public const MODEL = 'model';
+    public const PERMISSIONS = 'permissions';
+}
