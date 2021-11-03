42a62d8974690ef659db84edec9aff4633afb21b
Artyom Kuzminskiy <akaxiro@gmail.ru>
2021-11-03 16:53:34 +0500

    renameColumns and create new created_by_id and created_by_name

---
+++ database/migrations/2021_11_03_104755_change_name_columns_created_by_id_and_created_by_name_to_problem_cases_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeNameColumnsCreatedByIdAndCreatedByNameToProblemCasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('problem_cases', function (Blueprint $table) {
+            $table->renameColumn('created_by_id','post_or_pre_id');
+            $table->renameColumn('created_by_name','post_or_pre_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('problem_cases', function (Blueprint $table) {
+            $table->renameColumn('post_or_pre_id','created_by_id');
+            $table->renameColumn('post_or_pre_name','created_by_name');
+        });
+    }
+}
