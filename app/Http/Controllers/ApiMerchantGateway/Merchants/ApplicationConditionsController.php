b85493de5008da682bfe843df6e3b806ea423db9
orejametov <orejametov777@gmail.com>
2021-11-09 14:20:41 +0500

    small fix

--- app/Http/Controllers/ApiMerchantGateway/Merchants/ApplicationConditionsController.php
+++ app/Http/Controllers/ApiMerchantGateway/Merchants/ApplicationConditionsController.php
@@ -12,13 +12,18 @@ use Illuminate\Support\Facades\Cache;
 
 class ApplicationConditionsController extends ApiBaseController
 {
-    public function index1(Request $request)
+    public function index(Request $request)
     {
-        return Cache::tags($this->merchant_id)->remember($request->fullUrl(), 24 * 60, function () use ($request) {
+        return Cache::tags($this->merchant_id)->remember($request->fullUrl() . $this->store_id, 24 * 60, function () use ($request) {
+
+            $store = Store::findOrFail($this->store_id);
+            $special_conditions = $store->conditions;
+
             if ($request->has('post_alifshop') AND $request->query('post_alifshop') == true) {
                 $conditionQuery = Condition::query()
                     ->active()
                     ->where('post_alifshop', true)
+                    ->where('is_special', false)
                     ->byMerchant($this->merchant_id)
                     ->filterRequest($request)
                     ->orderRequest($request);
@@ -26,31 +31,14 @@ class ApplicationConditionsController extends ApiBaseController
                 $conditionQuery = Condition::query()
                     ->active()
                     ->where('post_merchant', true)
+                    ->where('is_special', false)
                     ->byMerchant($this->merchant_id)
                     ->filterRequest($request)
                     ->orderRequest($request);
             }
 
-            return $conditionQuery->get();
-        });
-    }
+            return array_merge($conditionQuery->get()->toArray(), $special_conditions->toArray());
 
-    public function index(Request $request)
-    {
-        return Cache::tags($this->merchant_id)->remember($request->fullUrl() . $this->store_id, 2 * 60, function () use ($request) {
-
-            $store = Store::findOrFail($this->store_id);
-
-            $special_conditions = $store->conditions;
-
-            $conditions = Condition::query()
-                ->active()
-                ->where('is_special', false)
-                ->byMerchant($this->merchant_id)
-                ->filterRequest($request)
-                ->orderRequest($request);
-
-            return array_merge($conditions->get()->toArray(), $special_conditions->toArray());
         });
     }
 }
