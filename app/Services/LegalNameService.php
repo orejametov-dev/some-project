df25fc66900ea04ed690ab210b4b275c0f290b9d
Artyom Kuzminskiy <akaxiro@gmail.ru>
2021-11-10 11:55:13 +0500

    add Family Proprietorship to LegalNameService.php list

--- app/Services/LegalNameService.php
+++ app/Services/LegalNameService.php
@@ -65,6 +65,16 @@ class LegalNameService
                 'description' => 'Частное Предприятие'
             ]
         ],
+        'FP' => [
+            'body_uz' => [
+                'value' => 'OK',
+                'description' => 'Oilaviy Korxona'
+            ],
+            'body_ru' => [
+                'value' => 'СП',
+                'description' => 'Семейное предприятие'
+            ]
+        ]
 
     ];
 
