09b46becffd248e04ff1587c550056687b29c88b
orejametov <orejametov777@gmail.com>
2021-11-22 17:52:19 +0500

    small fix

--- database/migrations/2021_11_22_053207_add_user_name_and_phone_columns_to_alifshop_merchant_access_table.php
+++ database/migrations/2021_11_22_053207_add_user_name_and_phone_columns_to_alifshop_merchant_access_table.php
@@ -14,7 +14,7 @@ class AddUserNameAndPhoneColumnsToAlifshopMerchantAccessTable extends Migration
     public function up()
     {
         Schema::table('alifshop_merchant_accesses', function (Blueprint $table) {
-            $table->unsignedBigInteger('user_id')->after('store_id')->unique();
+            $table->unsignedBigInteger('user_id')->after('store_id')->unique()->nullable();
             $table->string('user_name')->after('user_id')->nullable();
             $table->string('phone')->nullable()->after('user_name');
         });
