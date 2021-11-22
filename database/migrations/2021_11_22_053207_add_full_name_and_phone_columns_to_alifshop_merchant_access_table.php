81afd6f5752d41c8bb2f2edfea75cb16847337a0
Artyom Kuzminskiy <akaxiro@gmail.com>
2021-11-22 11:43:18 +0500

    add new columns user_name , user_id and phone

---
+++ database/migrations/2021_11_22_053207_add_full_name_and_phone_columns_to_alifshop_merchant_access_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddFullNameAndPhoneColumnsToAlifshopMerchantAccessTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('alifshop_merchant_accesses', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id')->after('store_id')->unique();
+            $table->string('user_name')->after('user_id')->nullable();
+            $table->string('phone')->nullable()->after('user_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('alifshop_merchant_accesses', function (Blueprint $table) {
+            //
+        });
+    }
+}
