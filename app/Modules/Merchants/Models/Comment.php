8a47d0cd33e7fe2ed059cb31e3a18e59ecae3fe7
Artyom Kuzminskiy <akaxiro@gmail.com>
2021-12-02 15:48:38 +0500

    refactor Model Comment

--- app/Modules/Merchants/Models/Comment.php
+++ app/Modules/Merchants/Models/Comment.php
@@ -2,22 +2,38 @@
 
 namespace App\Modules\Merchants\Models;
 
+use App\Traits\SortableByQueryParams;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
+/**
+ * @property string $body
+ * @property string $commentable_type
+ * @property string $commentable_id
+ * @method static Builder|Comment filterRequest(Request $request)
+ * @method static Builder|Comment orderRequest(Request $request, string $default_order_str = 'id:desc')
+ * @method static Builder|Comment query()
+ */
 class Comment extends Model
 {
+    const PROBLEM_CASE_FOR_PRM = 'problem_case_for_prm';
+    const PROBLEM_CASE_FOR_MERCHANT = 'problem_case_for_merchant';
+
     use HasFactory;
+    use SortableByQueryParams;
+
     protected $table = 'comments';
 
     protected $fillable = [
         'body',
-        'from_manager',
-        'from_merchant'
+        'commentable_type',
+        'created_by_name'
     ];
 
-    public function problem_cases()
+    public function commentable()
     {
-        return $this->morphToMany(ProblemCase::class, 'comment', 'problem_case_comment', 'comment_id', 'problem_case_id');
+        return $this->morphTo();
     }
 }
