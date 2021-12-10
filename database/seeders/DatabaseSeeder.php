4cc26292d2bf4f4dc9bc66cecf54560938144562
Artyom Kuzminskiy <akaxiro@gmail.com>
2021-12-10 10:36:33 +0500

    add new seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,20 +16,15 @@ class DatabaseSeeder extends Seeder
     {
        $this->call([
            WebServicesSeeder::class,
-//           CompaniesSeeder::class,
-//           CompanyUsersSeeder::class,
            MerchantsSeeder::class,
            StoresSeeder::class,
            MerchantUsersSeeder::class,
            MerchantInfosSeeder::class,
            MerchantAdditionalAgreementsSeeder::class,
-//           MerchantRequestsSeeder::class,
            ApplicationConditionsSeeder::class,
            MerchantTags::class,
            MerchantTag::class,
-           AlifshopMerchantSeeder::class,
-//           AlifshopMerchantStoreSeeder::class,
-           AlifshopMerchantAccessSeeder::class
+           CompetitorsSeeder::class
        ]);
     }
 }
