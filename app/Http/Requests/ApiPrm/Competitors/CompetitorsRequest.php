c10c8cd91a368183d40c183fb8664703dd9021ca
Artyom Kuzminskiy <akaxiro@gmail.com>
2021-12-15 11:04:54 +0500

    create CompetitorsRequest.php

---
+++ app/Http/Requests/ApiPrm/Competitors/CompetitorsRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\ApiPrm\Competitors;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CompetitorsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'competitor_id' => 'required|integer',
+            'volume_sales' => 'required|integer',
+            'percentage_approve' => 'required|integer',
+            'partnership_at' => 'required|date'
+        ];
+    }
+}
