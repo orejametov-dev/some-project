b2ff20408f7fd91a1d9c50ae493f9476fa064d84
orejametov <orejametov777@gmail.com>
2022-01-10 15:41:19 +0500

    added event_id

--- app/Http/Resources/ApiGate/Conditions/ConditionsResource.php
+++ app/Http/Resources/ApiGate/Conditions/ConditionsResource.php
@@ -18,6 +18,7 @@ class ConditionsResource extends JsonResource
             'id' => $this->id,
             'merchant_id' => $this->merchant_id,
             'store_id' => $this->store_id,
+            'event_id' => $this->event_id,
             'commission' => $this->commission,
             'duration' => $this->duration,
             'discount' => $this->discount
