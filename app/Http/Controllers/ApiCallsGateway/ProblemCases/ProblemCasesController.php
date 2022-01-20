2da7d54c1344074fb49d5f2f1d8aeb124b9ea792
Kuzminskiy Artyom <akaxiro@gmail.com>
2022-01-20 13:44:13 +0500

    refactor

--- app/Http/Controllers/ApiCallsGateway/ProblemCases/ProblemCasesController.php
+++ app/Http/Controllers/ApiCallsGateway/ProblemCases/ProblemCasesController.php
@@ -4,24 +4,13 @@
 namespace App\Http\Controllers\ApiCallsGateway\ProblemCases;
 
 
-use App\Exceptions\ApiBusinessException;
 use App\Http\Controllers\ApiCallsGateway\ApiBaseController;
 use App\Http\Requests\ApiPrm\Merchants\ProblemCases\ProblemCaseStoreRequest;
-use App\Http\Resources\ApiCallsGateway\ProblemCases\ProblemCaseResource;
-use App\HttpServices\Core\CoreService;
-use App\HttpServices\Hooks\DTO\HookData;
-use App\HttpServices\Notify\NotifyMicroService;
-use App\Jobs\SendHook;
 use App\Modules\Merchants\DTO\ProblemCases\ProblemCaseDTO;
-use App\Jobs\SendSmsJob;
 use App\Modules\Merchants\Models\ProblemCase;
-use App\Modules\Merchants\Services\ProblemCases\ProblemCaseService;
-use App\Services\SMS\SmsMessages;
 use App\UseCases\ApiCallsGateway\ProblemCases\IndexProblemCasesUseCase;
 use App\UseCases\ApiCallsGateway\ProblemCases\StoreProblemCasesUseCase;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
-use function GuzzleHttp\Psr7\str;
 
 class ProblemCasesController extends ApiBaseController
 {
