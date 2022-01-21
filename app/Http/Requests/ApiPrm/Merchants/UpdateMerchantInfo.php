cd24957ce85f92f28accf17019e3e73456d9d503
orejametov <orejametov777@gmail.com>
2022-01-21 16:52:40 +0500

    added update merchant info request

---
+++ app/Http/Requests/ApiPrm/Merchants/UpdateMerchantInfo.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests\ApiPrm\Merchants;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateMerchantInfo extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'director_name' => 'required|max:255',
+            'phone' => 'required|digits:12',
+            'vat_number' => 'required|digits:12',
+            'mfo' => 'required|digits:5',
+            'tin' => 'required|digits:9',
+            'oked' => 'required|digits:5',
+            'address' => 'required',
+            'bank_account' => 'required|digits:20',
+            'bank_name' => 'required|max:255',
+        ];
+    }
+}
