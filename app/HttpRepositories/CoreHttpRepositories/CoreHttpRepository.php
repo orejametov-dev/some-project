95bebc950ba2e9c317bb9aa26ae33776c4fcc666
Kuzminskiy Artyom <akaxiro@gmail.com>
2022-01-21 14:55:21 +0500

    small fix

--- app/HttpRepositories/CoreHttpRepositories/CoreHttpRepository.php
+++ app/HttpRepositories/CoreHttpRepositories/CoreHttpRepository.php
@@ -2,8 +2,8 @@
 
 namespace App\HttpRepositories\CoreHttpRepositories;
 
-use App\HttpRepositories\CoreHttpResponse\ApplicationDataResponse;
 use App\HttpRepositories\CoreHttpResponse\ApplicationDataByContractNumberResponse;
+use App\HttpRepositories\CoreHttpResponse\ApplicationDataResponse;
 use Carbon\Carbon;
 use Illuminate\Http\Client\PendingRequest;
 use Illuminate\Support\Facades\Http;
@@ -33,39 +33,39 @@ class CoreHttpRepository
         $data = $this->getHttpClient()->get("applications/$contract_number")->throw()->json();
 
         return new ApplicationDataResponse(
-            id: (int) $data['id'],
-            merchant_id: (int) $data['merchant_id'],
-            store_id: (int) $data['store_id'],
-            client_id: (int) $data['client']['id'],
-            client_name: (string) $data['client']['name'],
-            client_surname: (string) $data['client']['surname'],
-            client_patronymic: (string) $data['client']['patronymic'],
-            phone: (string) $data['client']['phone'],
-            application_items: (array) $data['application_items'],
-            post_or_pre_created_by_id: (int) $data['merchant_engaged_by']['id'],
-            post_or_pre_created_by_name: (string) $data['merchant_engaged_by']['name'],
+            id: (int)$data['id'],
+            merchant_id: (int)$data['merchant_id'],
+            store_id: (int)$data['store_id'],
+            client_id: (int)$data['client']['id'],
+            client_name: (string)$data['client']['name'],
+            client_surname: (string)$data['client']['surname'],
+            client_patronymic: (string)$data['client']['patronymic'],
+            phone: (string)$data['client']['phone'],
+            application_items: (array)$data['application_items'],
+            post_or_pre_created_by_id: (int)$data['merchant_engaged_by']['id'],
+            post_or_pre_created_by_name: (string)$data['merchant_engaged_by']['name'],
             credit_contract_date: Carbon::parse($data['contract_date'])
         );
     }
 
     public function getApplicationDataByApplicationId($application_id)
     {
-        $data =  $this->getHttpClient()->get("applications/$application_id")->throw()->json();
+        $data = $this->getHttpClient()->get("applications/$application_id")->throw()->json();
 
         return new ApplicationDataResponse(
-        id: (int) $data['id'],
-        merchant_id: (int) $data['merchant_id'],
-        store_id: (int) $data['store_id'],
-        client_id: (int) $data['client']['id'],
-        client_name: (string) $data['client']['name'],
-        client_surname: (string) $data['client']['surname'],
-        client_patronymic: (string) $data['client']['patronymic'],
-        phone: (string) $data['client']['phone'],
-        application_items: (array) $data['application_items'],
-        post_or_pre_created_by_id: (int) $data['merchant_engaged_by']['id'],
-        post_or_pre_created_by_name: (string) $data['merchant_engaged_by']['name'],
-        application_created_at: Carbon::parse($data['created_at'])
-    );
+            id: (int)$data['id'],
+            merchant_id: (int)$data['merchant_id'],
+            store_id: (int)$data['store_id'],
+            client_id: (int)$data['client']['id'],
+            client_name: (string)$data['client']['name'],
+            client_surname: (string)$data['client']['surname'],
+            client_patronymic: (string)$data['client']['patronymic'],
+            phone: (string)$data['client']['phone'],
+            application_items: (array)$data['application_items'],
+            post_or_pre_created_by_id: (int)$data['merchant_engaged_by']['id'],
+            post_or_pre_created_by_name: (string)$data['merchant_engaged_by']['name'],
+            application_created_at: Carbon::parse($data['created_at'])
+        );
     }
 
     public function getAmountOfMerchantSales()
