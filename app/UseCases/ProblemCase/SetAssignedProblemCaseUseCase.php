6b94bd057427420054ac0dd4c78a460cba598e4b
Kuzminskiy Artyom <akaxiro@gmail.com>
2022-01-22 10:47:33 +0500

    create UseCase

---
+++ app/UseCases/ProblemCase/SetAssignedProblemCaseUseCase.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\UseCases\ProblemCase;
+
+use App\Exceptions\BusinessException;
+use App\Modules\Merchants\Models\ProblemCase;
+
+class SetAssignedProblemCaseUseCase
+{
+    public function execute(int $id , int $assigned_to_id , string $assigned_to_name): ProblemCase
+    {
+        $problemCase = ProblemCase::query()->find($id);
+
+        if ($problemCase === null) {
+            throw new BusinessException('Проблемный кейс не найден', 'problem_case_not_exists', 404);
+        }
+
+        $problemCase->assigned_to_id = $assigned_to_id;
+        $problemCase->assigned_to_name = $assigned_to_name;
+        $problemCase->save();
+
+        return $problemCase;
+    }
+}
---
+++ app/UseCases/ProblemCase/UpdateProblemCaseUseCase.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\UseCases\ProblemCase;
+
+use App\Exceptions\BusinessException;
+use App\Modules\Merchants\Models\ProblemCase;
+use Carbon\Carbon;
+
+class UpdateProblemCaseUseCase
+{
+    public function execute(int $id , Carbon $deadline): ProblemCase
+    {
+        $problemCase = ProblemCase::query()->find($id);
+
+        if ($problemCase === null) {
+            throw new BusinessException('Проблемный кейс не найден' , 'problem_case_not_exists' , 404);
+        }
+
+        $problemCase->deadline = Carbon::parse($deadline)->format('Y-m-d');
+        $problemCase->save();
+
+        return $problemCase;
+    }
+}
