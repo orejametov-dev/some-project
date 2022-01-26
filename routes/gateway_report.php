d8a5feab03bdce28e186e154a306360b39262625
orejametov <orejametov777@gmail.com>
2022-01-26 10:15:33 +0500

    small fix

--- routes/gateway_report.php
+++ routes/gateway_report.php
@@ -1,13 +1,10 @@
 <?php
 
-use App\Http\Controllers\ApiOnlineGateway\Conditions\ConditionsController;
-use App\Http\Controllers\ApiOnlineGateway\Merchants\MerchantsController as OnlineMerchantsController;
-use App\Http\Controllers\ApiOnlineGateway\Stores\StoresController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::prefix('merchants')
     ->group(function () {
-        Route::get('/', [OnlineMerchantsController::class, 'index']);
+        Route::get('/', [\App\Http\Controllers\ApiReportGateway\Merchants\MerchantsController::class, 'index']);
     });
 
