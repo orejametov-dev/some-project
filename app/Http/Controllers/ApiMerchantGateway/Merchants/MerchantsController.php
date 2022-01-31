30b5e326363f42c00d54596de45ba8f767f38e42
orejametov <orejametov777@gmail.com>
2022-01-31 22:48:11 +0500

    small fix

--- app/Http/Controllers/ApiMerchantGateway/Merchants/MerchantsController.php
+++ app/Http/Controllers/ApiMerchantGateway/Merchants/MerchantsController.php
@@ -37,7 +37,7 @@ class MerchantsController extends ApiBaseController
         $azo_merchant_access = $this->azo_merchant_access;
 
         $store = Cache::tags($this->merchant_id)->remember($azo_merchant_access->id.'detail_cache_of_merchant_stores', 60 * 60, function () {
-            $azo_merchant_access = AzoMerchantAccess::query()->byUserId($this->user->id)->firstOrFail();
+            $azo_merchant_access = AzoMerchantAccess::query()->byUserId($this->user->getId())->firstOrFail();
             return Store::query()
                 ->findOrFail($azo_merchant_access->store_id);
         });
