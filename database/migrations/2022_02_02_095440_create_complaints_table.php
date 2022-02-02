597db3ea007d5989ca8f9b2bf954c21a6370eb9a
Kuzminskiy Artyom <akaxiro@gmail.com>
2022-02-02 16:57:37 +0500

    create migrate complaints

---
+++ database/migrations/2022_02_02_095440_create_complaints_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComplaintsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('complaints', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('name');
+            $table->string('surname')->nullable();
+            $table->string('patronymic')->nullable();
+            $table->string('reason_correction');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('complaints');
+    }
+}
