462a74c5abbc175d34721ae256efbda320f684ef
Kuzminskiy Artyom <akaxiro@gmail.com>
2022-02-02 16:58:02 +0500

    create ComplaintsController.php

---
+++ app/Http/Controllers/ApiGateway/AzoMerchants/Complaints/ComplaintsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\ApiGateway\AzoMerchants\Complaints;
+
+use App\Http\Controllers\Controller;
+use App\Modules\Merchants\Models\Complaint;
+use Illuminate\Http\Request;
+
+class ComplaintsController extends Controller
+{
+    public function index(Request $request)
+    {
+        $complaints = Complaint::query()
+            ->filterRequest($request)
+            ->orderRequest($request);
+
+        return $complaints->paginate($request->input('per_page') ?? 15);
+    }
+}
