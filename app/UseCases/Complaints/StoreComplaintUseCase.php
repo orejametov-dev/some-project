42ab295f8f2fdf915fe7b6f2f6e509ebc953993f
Kuzminskiy Artyom <akaxiro@gmail.com>
2022-02-03 16:43:24 +0500

    small fix

--- app/UseCases/Complaints/StoreComplaintUseCase.php
+++ app/UseCases/Complaints/StoreComplaintUseCase.php
@@ -18,7 +18,7 @@ class StoreComplaintUseCase
         }
 
         $complaint = new Complaint();
-        $complaint->azo_merchant_access_id = $storeComplaintDTO->user_id;
+        $complaint->azo_merchant_access_id = $merchant_access->id;
         $complaint->reason_correction = $storeComplaintDTO->reason_correction;
 
         $complaint->save();
