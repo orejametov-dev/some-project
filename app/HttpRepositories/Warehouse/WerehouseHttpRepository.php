31660cc18e487585512f6fa2be824d8ed2f449ea
Kuzminskiy Artyom <akaxiro@gmail.com>
2022-02-04 15:47:16 +0500

    create WerehouseHttpRepository.php

---
+++ app/HttpRepositories/Warehouse/WerehouseHttpRepository.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\HttpRepositories\Warehouse;
+
+use App\Exceptions\BusinessException;
+use Illuminate\Http\Client\PendingRequest;
+use Illuminate\Support\Facades\Http;
+
+class WerehouseHttpRepository
+{
+    public function checkDuplicateSKUs($merchant_id)
+    {
+        $response = $this->getHttpClient()->get('/gate/items/check-duplications', [
+            'merchant_id' => $merchant_id
+        ]);
+        if($response->clientError()) {
+            throw new BusinessException($response->body(), 'duplicate_body');
+        }
+
+    }
+
+    protected function getHttpClient(): PendingRequest
+    {
+        ;
+        return Http::baseUrl(config('local_services.warehouse.domain') . '/')
+            ->withHeaders([
+                'Accept' => 'application/json',
+                'Access-Token' => config('local_services.warehouse.access_token')
+            ]);
+    }
+}
