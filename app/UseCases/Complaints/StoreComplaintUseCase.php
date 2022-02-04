709c85403f01da3b50631e2c56501afde80f4be3
Kuzminskiy Artyom <akaxiro@gmail.com>
2022-02-04 10:31:41 +0500

    use FindNerchantUserUseCase

--- app/UseCases/Complaints/StoreComplaintUseCase.php
+++ app/UseCases/Complaints/StoreComplaintUseCase.php
@@ -6,16 +6,19 @@ use App\DTOs\Complaints\StoreComplaintDTO;
 use App\Exceptions\BusinessException;
 use App\Modules\Merchants\Models\AzoMerchantAccess;
 use App\Modules\Merchants\Models\Complaint;
+use App\UseCases\MerchantUsers\FindMerchantUserUseCase;
 
 class StoreComplaintUseCase
 {
+    public function __construct(
+        private FindMerchantUserUseCase $findMerchantUserUseCase
+    )
+    {
+    }
+
     public function execute(StoreComplaintDTO $storeComplaintDTO): Complaint
     {
-        $merchant_access = AzoMerchantAccess::query()->find($storeComplaintDTO->user_id);
-
-        if ($merchant_access === null) {
-            throw new BusinessException('Пользователь не найден' , 'object_not_found' , 404);
-        }
+        $merchant_access = $this->findMerchantUserUseCase->execute($storeComplaintDTO->user_id);
 
         $complaint = new Complaint();
         $complaint->azo_merchant_access_id = $merchant_access->id;
