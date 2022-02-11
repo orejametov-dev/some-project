26edf8455a459d55c51da9ea39a4d2ce3f242592
Iskandar Rakhimov <iskandarrakhimov@Iskandars-MacBook-Air.local>
2022-02-11 23:44:54 +0500

    added linters

--- Makefile
+++ Makefile
@@ -7,7 +7,25 @@ down:
 	make docker-compose COMMAND="down"
 docker-compose:
 	docker-compose -f ./infra/local/docker-compose.yml --env-file infra/local/.env --project-name alif-merchant-service ${COMMAND}
+
+#composer
 install:
-	make run-in-app COMMAND="composer i"
-run-in-app:
+	make run-on-image COMMAND="composer i"
+update:
+	make run-on-image COMMAND="composer u"
+require:
+	make run-on-image COMMAND="composer require ${name}"
+
+# linters
+cs-check:
+	make run-inside-container COMMAND="./vendor/bin/php-cs-fixer fix -vvv --dry-run --show-progress=dots --config=.php-cs-fixer.php --allow-risky=yes"
+cs-fix:
+	make run-inside-container COMMAND="./vendor/bin/php-cs-fixer fix -vvv --show-progress=dots --config=.php-cs-fixer.php --allow-risky=yes"
+analyse:
+	make run-inside-container COMMAND="./vendor/bin/phpstan analyse --memory-limit=-1G"
+
+run-inside-container:
 	docker exec -it alif-service-merchant-app ${COMMAND}
+
+run-on-image:
+	docker run --rm -v "${PWD}"/:/app alif-merchant-service_app ${COMMAND}
--- app/Console/Commands/Settings/WriteLogsToTable.php
+++ app/Console/Commands/Settings/WriteLogsToTable.php
@@ -9,60 +9,60 @@ use Illuminate\Support\Facades\Cache;
 
 class WriteLogsToTable extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'logs:writing';
-
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Writing logs to table logs every ten minutes';
-
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    private $logs;
-
-    public function __construct()
-    {
-        parent::__construct();
-        $this->logs = Cache::get(TimeLogger::CACHE_KEY);
-    }
-
-    /**
-     * Execute the console command.
-     *
-     * @return int
-     */
-    public function handle()
-    {
-        \Log::channel('command')->info(self::class . '|' . now() . ':' . 'started');
-
-        try {
-            if (!empty($this->logs)) {
-                foreach (array_chunk($this->logs, 10000) as $chunk_logs) {
-                    Log::query()->insert($chunk_logs);
-                }
-
-                Cache::forget(TimeLogger::CACHE_KEY);
-            }
-        } catch (\Exception $e) {
-            if (app()->bound('sentry')) {
-                app('sentry')->captureException($e);
-            }
-
-            Cache::forget(TimeLogger::CACHE_KEY);
-        }
-
-        \Log::channel('command')->info(self::class . '|' . now() . ':' . 'finished');
-
-        return 0;
-    }
+//    /**
+//     * The name and signature of the console command.
+//     *
+//     * @var string
+//     */
+//    protected $signature = 'logs:writing';
+//
+//    /**
+//     * The console command description.
+//     *
+//     * @var string
+//     */
+//    protected $description = 'Writing logs to table logs every ten minutes';
+//
+//    /**
+//     * Create a new command instance.
+//     *
+//     * @return void
+//     */
+//    private $logs;
+//
+//    public function __construct()
+//    {
+//        parent::__construct();
+//        $this->logs = Cache::get(TimeLogger::CACHE_KEY);
+//    }
+//
+//    /**
+//     * Execute the console command.
+//     *
+//     * @return int
+//     */
+//    public function handle()
+//    {
+//        \Log::channel('command')->info(self::class . '|' . now() . ':' . 'started');
+//
+//        try {
+//            if (!empty($this->logs)) {
+//                foreach (array_chunk($this->logs, 10000) as $chunk_logs) {
+//                    Log::query()->insert($chunk_logs);
+//                }
+//
+//                Cache::forget(TimeLogger::CACHE_KEY);
+//            }
+//        } catch (\Exception $e) {
+//            if (app()->bound('sentry')) {
+//                app('sentry')->captureException($e);
+//            }
+//
+//            Cache::forget(TimeLogger::CACHE_KEY);
+//        }
+//
+//        \Log::channel('command')->info(self::class . '|' . now() . ':' . 'finished');
+//
+//        return 0;
+//    }
 }
