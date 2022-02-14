4af0000165f7aecee1cc0846ba9d5ba21cf09ea5
Kuzminskiy Artyom <akaxiro@gmail.com>
2022-02-14 09:34:36 +0500

    add new filter from merchant_id

--- app/Filters/ProblemCase/ProblemCaseFilters.php
+++ app/Filters/ProblemCase/ProblemCaseFilters.php
@@ -7,6 +7,7 @@ use App\Filters\CommonFilters\ClientIdFilter;
 use App\Filters\CommonFilters\CreatedAtFilter;
 use App\Filters\CommonFilters\DateFilter;
 use App\Filters\CommonFilters\IdFilter;
+use App\Filters\CommonFilters\MerchantIdFilter;
 use App\Filters\CommonFilters\MerchantIdsFilter;
 use App\Filters\CommonFilters\StatusIdFilter;
 use App\Filters\CommonFilters\StoreIdsFilter;
@@ -26,6 +27,7 @@ class ProblemCaseFilters extends AbstractFilters
         CreatedFromNameFilter::class,
         EngagedByIdFilter::class,
         ProblemCaseTagIdFilter::class,
+        MerchantIdFilter::class,
     ];
 
     protected function getRequestBindings(): array
