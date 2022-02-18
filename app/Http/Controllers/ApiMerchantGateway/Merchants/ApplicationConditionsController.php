48e20afb15e7a2875079470b96715282e37af026
Kuzminskiy Artyom <akaxiro@gmail.com>
2022-02-18 15:35:44 +0500

    fix filter

--- app/Http/Controllers/ApiMerchantGateway/Merchants/ApplicationConditionsController.php
+++ app/Http/Controllers/ApiMerchantGateway/Merchants/ApplicationConditionsController.php
@@ -26,7 +26,7 @@ class ApplicationConditionsController extends ApiBaseController
                     ->where('post_alifshop', true)
                     ->where('is_special', false)
                     ->byMerchant($this->merchant_id)
-                    ->filterRequest($request)
+                    ->filterRequests($request)
                     ->orderRequest($request);
             } else {
                 $special_conditions = $store->conditions()
@@ -39,7 +39,7 @@ class ApplicationConditionsController extends ApiBaseController
                     ->where('post_merchant', true)
                     ->where('is_special', false)
                     ->byMerchant($this->merchant_id)
-                    ->filterRequest($request)
+                    ->filterRequests($request)
                     ->orderRequest($request);
             }
 
