b3a97384db0435f19322f4efc65a393b9bfa372e
Kuzminskiy Artyom <akaxiro@gmail.com>
2022-02-21 09:42:54 +0500

    fix filter

--- app/Filters/CommonFilters/MerchantIdsFilter.php
+++ app/Filters/CommonFilters/MerchantIdsFilter.php
@@ -10,7 +10,9 @@ class MerchantIdsFilter extends AbstractExactFilter
     public function filter(Builder $builder, mixed $value): void
     {
         $merchant_ids = explode(';', $value);
-        $builder->whereIn('id', $merchant_ids);
+        $builder->whereHas('merchant', function ($builder) use ($merchant_ids) {
+            $builder->whereIn('id', $merchant_ids);
+        });
     }
 
     public function getBindingName(): string
