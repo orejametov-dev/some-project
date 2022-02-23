8adc5a1c2115179c32261ca6e3c7d75d8d1e2713
Kuzminskiy Artyom <akaxiro@gmail.com>
2022-02-23 16:39:12 +0500

    create new filter

---
+++ app/Filters/Notification/FreshFilter.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filters\Notification;
+
+use App\Filters\AbstractExactFilter;
+use Illuminate\Database\Eloquent\Builder;
+
+class FreshFilter extends AbstractExactFilter
+{
+
+    public function filter(Builder $builder, mixed $value): void
+    {
+        if ($value == true) {
+            $builder->where('start_schedule', '<=', now())
+                ->where('end_schedule', '>=', now());
+        }
+    }
+
+    public function getBindingName(): string
+    {
+        return 'fresh';
+    }
+}
