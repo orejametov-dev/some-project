f9c3e59d74abb85b630f6c357769127ca52faeec
Kuzminskiy Artyom <akaxiro@gmail.com>
2022-02-24 16:22:33 +0500

    refactor store merchant info

--- app/DTOs/MerchantInfos/StoreMerchantInfoDTO.php
+++ app/DTOs/MerchantInfos/StoreMerchantInfoDTO.php
@@ -13,8 +13,6 @@ class StoreMerchantInfoDTO
     public function __construct(
         public int $merchant_id,
         public string $director_name,
-        public string $legal_name,
-        public string $legal_name_prefix,
         public string $phone,
         public string $vat_number,
         public string $mfo,
@@ -31,8 +29,7 @@ class StoreMerchantInfoDTO
         return new self(
             self::parseInt($data['merchant_id']),
             self::parseString($data['director_name']),
-            self::parseString($data['legal_name']),
-            self::parseString($data['legal_name_prefix']),
+
             self::parseString($data['phone']),
             self::parseString($data['vat_number']),
             self::parseString($data['mfo']),
