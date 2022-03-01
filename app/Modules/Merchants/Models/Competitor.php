7be027a26d26541ca2b72007bbc628734084c928
Kuzminskiy Artyom <akaxiro@gmail.com>
2022-03-01 13:35:23 +0500

    add docs

--- app/Modules/Merchants/Models/Competitor.php
+++ app/Modules/Merchants/Models/Competitor.php
@@ -4,13 +4,17 @@ namespace App\Modules\Merchants\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use PhpParser\Builder;
 
+/**
+ * @method static Builder|Competitor query()
+ */
 class Competitor extends Model
 {
     use HasFactory;
 
     public function merchants()
     {
-        return $this->belongsToMany(Merchant::class, 'merchant_competitor')->withPivot('volume_sales', 'percentage_approve', 'partnership_at')->withTimestamps();
+        return $this->belongsToMany(Merchant::class, 'merchant_competitor')->withPivot('percentage_sales', 'percentage_approve', 'partnership_at')->withTimestamps();
     }
 }
