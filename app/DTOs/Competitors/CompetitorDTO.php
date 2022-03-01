a16720fa2d6504a16fd8b1d6f2c2426b3259d40a
Kuzminskiy Artyom <akaxiro@gmail.com>
2022-03-01 13:35:33 +0500

    create DTO

---
+++ app/DTOs/Competitors/CompetitorDTO.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DTOs\Competitors;
+
+use Alifuz\Utils\Parser\ParseDataTrait;
+use Carbon\Carbon;
+
+class CompetitorDTO
+{
+    use ParseDataTrait;
+
+    /**
+     * @param int $merchant_id
+     * @param int $competitor_id
+     * @param int|null $percentage_sales
+     * @param int|null $percentage_approve
+     * @param Carbon|null $partnership_at
+     */
+    public function __construct(
+        public int $merchant_id,
+        public int $competitor_id,
+        public ?int $percentage_sales,
+        public ?int $percentage_approve,
+        public ?Carbon $partnership_at,
+    ) {
+    }
+
+    public static function fromArray(int $merchant_id, array $data): self
+    {
+        return new self(
+            self::parseInt($merchant_id),
+            self::parseInt($data['competitor_id']),
+            self::parseNullableInt($data['percentage_sales']),
+            self::parseNullableInt($data['percentage_approve']),
+            isset($data['partnership_at']) ? Carbon::parse($data['partnership_at']) : null,
+        );
+    }
+}
