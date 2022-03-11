4ca8a16e1ae760c49e5067f28919d0f1b99428ec
Kuzminskiy Artyom <akaxiro@gmail.com>
2022-03-11 16:52:07 +0500

    create CommentsController.php

---
+++ app/Http/Controllers/ApiCallsGateway/Comments/CommentsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\ApiCallsGateway\Comments;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ApiPrm\Comments\IndexComments;
+use App\Modules\Merchants\Models\Comment;
+
+class CommentsController extends Controller
+{
+    public function index(IndexComments $request)
+    {
+        $comments = Comment::query()
+            ->where('commentable_type', $request->input('commentable_type'))
+            ->where('commentable_id', $request->input('commentable_id'))
+            ->orderRequest($request);
+
+        return $comments->paginate($request->query('per_page') ?? 15);
+    }
+}
