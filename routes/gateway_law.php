55f6a8758aff1ed47a17bc17f8f248d3c666b611
Kuzminskiy Artyom <akaxiro@gmail.com>
2022-03-11 15:10:51 +0500

    fixed law route

--- routes/gateway_law.php
+++ routes/gateway_law.php
@@ -3,8 +3,8 @@
 declare(strict_types=1);
 
 use App\Http\Controllers\ApiLawGateway\ProblemCases\ProblemCasesController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiLawGateway\ProblemCases\ProblemCaseTagsController;
+use Illuminate\Support\Facades\Route;
 
 Route::prefix('merchants/problem-cases')
     ->group(function () {
