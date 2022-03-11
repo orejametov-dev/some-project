5d6a25e9f18ab21d8f466dfbf7bc24d637d1f0be
Kuzminskiy Artyom <akaxiro@gmail.com>
2022-03-11 15:09:32 +0500

    merge into master

--- routes/gateway_calls.php
+++ routes/gateway_calls.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use App\Http\Controllers\ApiCallsGateway\ProblemCases\ProblemCasesController;
+use App\Http\Controllers\ApiCallsGateway\ProblemCases\ProblemCaseTagsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::prefix('merchants/problem-cases')
     ->group(function () {
         Route::get('/', [ProblemCasesController::class, 'index']);
+        Route::get('/tags', [ProblemCaseTagsController::class, 'index']);
         Route::get('/statuses', [ProblemCasesController::class, 'getStatusList']);
         Route::get('/{id}', [ProblemCasesController::class, 'show']);
         Route::post('/', [ProblemCasesController::class, 'store']);
--- routes/gateway_law.php
+++ routes/gateway_law.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 use App\Http\Controllers\ApiLawGateway\ProblemCases\ProblemCasesController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiLawGateway\ProblemCases\ProblemCaseTagsController;
 
 Route::prefix('merchants/problem-cases')
     ->group(function () {
         Route::post('/', [ProblemCasesController::class, 'store']);
+        Route::get('/tags', [ProblemCaseTagsController::class, 'index']);
     });
