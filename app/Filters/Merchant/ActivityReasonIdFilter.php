ec919004cd424618176a237d11bb23a2444d781e
Kuzminskiy Artyom <akaxiro@gmail.com>
2022-03-14 10:42:18 +0500

    refactor filter

--- app/Filters/Merchant/ActivityReasonIdFilter.php
+++ app/Filters/Merchant/ActivityReasonIdFilter.php
@@ -4,13 +4,21 @@ namespace App\Filters\Merchant;
 
 use App\Filters\AbstractExactFilter;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\DB;
 
 class ActivityReasonIdFilter extends AbstractExactFilter
 {
     public function filter(Builder $builder, mixed $value): void
     {
         $builder->whereHas('merchant_activities', function (Builder $builder) use ($value) {
-            $builder->where('activity_reason_id', $value);
+            $builder->join(
+                DB::raw('(select merchant_activities.merchant_id as merchant_id, max(merchant_activities.id) as max_id from merchant_activities group by merchant_id) as sub_query'),
+                'sub_query.merchant_id',
+                '=',
+                'merchants.id'
+            )
+                ->whereRaw('sub_query.max_id = merchant_activities.id')
+                ->where('merchant_activities.activity_reason_id', $value);
         });
     }
 
