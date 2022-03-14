f8d8a38740988822078583ef647d1634606ab7ea
orejametov <orejametov777@gmail.com>
2022-03-14 14:02:31 +0500

    removed update current sales logic of merchant from command

--- app/Console/Commands/Settings/UpdateCurrentSales.php
+++ app/Console/Commands/Settings/UpdateCurrentSales.php
@@ -4,8 +4,7 @@ declare(strict_types=1);
 
 namespace App\Console\Commands\Settings;
 
-use App\HttpServices\Core\CoreService;
-use App\Modules\Merchants\Models\Merchant;
+use App\UseCases\Merchants\UpdateMerchantCurrentSalesUseCase;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
 
@@ -40,47 +39,11 @@ class UpdateCurrentSales extends Command
      *
      * @return int
      */
-    public function handle()
+    public function handle(UpdateMerchantCurrentSalesUseCase $updateMerchantCurrentSalesUseCase)
     {
         Log::channel('command')->info(self::class . '|' . now() . ':' . 'started');
 
-        $percentage_of_limit = Merchant::$percentage_of_limit;
-
-        $amount_of_merchants = CoreService::getAmountOfMerchantSales();
-
-        foreach ($amount_of_merchants as $amount_of_merchant) {
-            $merchant = Merchant::findOrFail($amount_of_merchant['merchant_id']);
-            $merchant->current_sales = $amount_of_merchant['discounted_amount'];
-            if ($merchant_info = $merchant->merchant_info) {
-                $total_limit = $merchant_info->limit;
-                $rest_limit = $merchant_info->limit - $merchant->current_sales;
-                $merchant_info->rest_limit = $rest_limit > 0 ? $rest_limit : 0;
-                if ($merchant->additional_agreements()->exists()) {
-                    $additional_agreements = $merchant->additional_agreements;
-                    foreach ($additional_agreements as $additional_agreement) {
-                        $total_limit += $additional_agreement->limit;
-                        $rest_limit = $total_limit - $merchant->current_sales;
-                        $additional_agreement->rest_limit = $rest_limit > 0 ? $rest_limit : 0;
-                        $additional_agreement->save();
-                    }
-                }
-                $merchant_info->save();
-            }
-            $merchant->save();
-        }
-
-        Merchant::query()
-            ->leftJoin('merchant_infos', 'merchants.id', '=', 'merchant_infos.merchant_id')
-            ->whereRaw("IFNULL(merchant_infos.limit,0) {$percentage_of_limit} <= merchants.current_sales")
-            ->whereNull('merchant_infos.limit_expired_at')
-            ->update(['merchant_infos.limit_expired_at' => now()]);
-
-        Merchant::query()
-            ->leftJoin('merchant_infos', 'merchants.id', '=', 'merchant_infos.merchant_id')
-            ->leftJoin('merchant_additional_agreements', 'merchants.id', '=', 'merchant_additional_agreements.merchant_id')
-            ->whereRaw("(IFNULL(merchant_infos.limit, 0) + IFNULL(merchant_additional_agreements.limit, 0)) $percentage_of_limit <= merchants.current_sales")
-            ->whereNull('merchant_additional_agreements.limit_expired_at')
-            ->update(['merchant_additional_agreements.limit_expired_at' => now()]);
+        $updateMerchantCurrentSalesUseCase->execute();
 
         Log::channel('command')->info(self::class . '|' . now() . ':' . 'finished');
 
