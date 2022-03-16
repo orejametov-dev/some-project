43a17c710ba7e92bf31c8f048fbc47ff83742dd2
Kuzminskiy Artyom <akaxiro@gmail.com>
2022-03-16 15:07:09 +0500

    create AmountOfMerchantSalesListResponse

--- app/HttpRepositories/HttpResponses/Core/AmountOfMerchantSalesResponse.php
+++ app/HttpRepositories/HttpResponses/Core/AmountOfMerchantSalesResponse.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App\HttpRepositories\HttpResponses\Core;
 
 use Alifuz\Utils\Parser\ParseDataTrait;
@@ -11,14 +9,26 @@ class AmountOfMerchantSalesResponse
     use ParseDataTrait;
 
     public function __construct(
-        public array $array,
+        private int $merchant_id,
+        private int $discounted_amount,
     ) {
     }
 
     public static function fromArray(array $data): self
     {
         return new self(
-            self::parseArray($data),
+            merchant_id: self::parseInt($data['merchant_id']),
+            discounted_amount: self::parseInt($data['discounted_amount'])
         );
+    }
+
+    public function getMerchantId(): int
+    {
+        return $this->merchant_id;
+    }
+
+    public function getDiscountedAmount(): int
+    {
+        return $this->discounted_amount;
     }
 }
