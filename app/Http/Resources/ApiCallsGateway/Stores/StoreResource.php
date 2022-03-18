70fafc71b3e8dd9030f6f1cebf3967358e72fa08
orejametov <orejametov777@gmail.com>
2022-03-18 16:37:19 +0500

    added route for calls for getting stores

---
+++ app/Http/Resources/ApiCallsGateway/Stores/StoreResource.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Resources\ApiCallsGateway\Stores;
+
+use App\Models\Merchant;
+use App\Models\Store;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class StoreResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        /** @var Store|StoreResource $this */
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'merchant_id' => $this->merchant_id,
+            'address' => $this->address,
+            'phone' => $this->phone,
+            'responsible_person' => $this->responsible_person
+        ];
+    }
+}
