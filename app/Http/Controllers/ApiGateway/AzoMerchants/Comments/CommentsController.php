133c46ffb8eae51e8f5cab43f0c2ccd410289690
Kuzminskiy Artyom <akaxiro@gmail.com>
2022-03-24 15:10:24 +0500

    add resource

--- app/Http/Controllers/ApiGateway/AzoMerchants/Comments/CommentsController.php
+++ app/Http/Controllers/ApiGateway/AzoMerchants/Comments/CommentsController.php
@@ -6,17 +6,19 @@ namespace App\Http\Controllers\ApiGateway\AzoMerchants\Comments;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ApiPrm\Comments\IndexComments;
+use App\Http\Resources\ApiGateway\Comments\IndexCommentResource;
 use App\Models\Comment;
+use Illuminate\Http\Resources\Json\JsonResource;
 
 class CommentsController extends Controller
 {
-    public function index(IndexComments $request)
+    public function index(IndexComments $request): JsonResource
     {
         $comments = Comment::query()
             ->where('commentable_type', $request->input('commentable_type'))
             ->where('commentable_id', $request->input('commentable_id'))
             ->orderRequest($request);
 
-        return $comments->paginate($request->query('per_page') ?? 15);
+        return IndexCommentResource::collection($comments->paginate($request->query('per_page') ?? 15));
     }
 }
