b179dcab8a083acf1b1e5bd8e71e7116ea3060ec
orejametov <orejametov777@gmail.com>
2022-03-24 22:04:16 +0500

    small fix

--- app/Models/Merchant.php
+++ app/Models/Merchant.php
@@ -211,9 +211,9 @@ class Merchant extends Model
         $storage_file = (new StorageHttpRepository)->uploadFile($uploadedFile, 'merchants');
         $merchant_file = new File();
         $merchant_file->file_type = $type;
-        $merchant_file->mime_type = $storage_file['mime_type'];
-        $merchant_file->size = $storage_file['size'];
-        $merchant_file->url = $storage_file['url'];
+        $merchant_file->mime_type = $storage_file->getMimeType();
+        $merchant_file->size = $storage_file->getSize();
+        $merchant_file->url = $storage_file->getUrl();
         $merchant_file->merchant_id = $this->id;
         $merchant_file->save();
 
