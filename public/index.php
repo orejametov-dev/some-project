cee9c9d1d1afda0270d9c299d8c91ec79a0fa5b4
orejametov <orejametov777@gmail.com>
2022-03-30 11:30:26 +0500

    small fix

--- public/index.php
+++ public/index.php
@@ -52,5 +52,5 @@ $kernel = $app->make(Kernel::class);
 $response = tap($kernel->handle(
     $request = Request::capture()
 ))->send();
-
+\Illuminate\Support\Facades\Log::info($request->fullUrl());
 $kernel->terminate($request, $response);
