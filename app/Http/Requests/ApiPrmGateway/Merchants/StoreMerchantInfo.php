ae5eaa9821ab7d2edfffbda0e4c62080b67335cb
iiskanr <i.rakhimov@aliftech.uz>
2022-04-05 11:56:43 +0500

    renamed folders

---
+++ app/Http/Requests/ApiPrmGateway/Merchants/StoreMerchantInfo.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests\ApiPrmGateway\Merchants;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreMerchantInfo extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'merchant_id' => 'required|integer',
+            'director_name' => 'required|max:255',
+            'phone' => 'required|digits:12',
+            'vat_number' => 'required|digits:12',
+            'mfo' => 'required|digits:5',
+            'tin' => 'required|digits:9',
+            'oked' => 'required|digits:5',
+            'address' => 'required',
+            'bank_account' => 'required|digits:20',
+            'bank_name' => 'required|max:255',
+        ];
+    }
+}
---
+++ app/Http/Requests/ApiPrmGateway/Stores/StoreStoresRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests\ApiPrmGateway\Stores;
+
+use App\Services\RegionService;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreStoresRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|max:255',
+            'merchant_id' => 'required|numeric',
+            'address' => 'nullable|string',
+            'responsible_person' => 'required_with:responsible_person_phone|nullable|string',
+            'responsible_person_phone' => 'required_with:responsible_person|nullable|digits:12',
+            'region' => [
+                'required',
+                Rule::in(RegionService::getKeys()),
+            ],
+            'district' => 'required|string',
+        ];
+    }
+}
